553ab2ee8f63932ca4832e87a464f9644ee2409a
Julien Motaharian <jmotaharian1@gmail.com>
2025-06-09 14:11:28 -0400

    added comments for each method; also referenced the chat gpt citation

--- model_utils.py
+++ model_utils.py
@@ -1,6 +1,16 @@
 import pandas as pd
 
 def normalize_misinfo_dataset(file_path: str) -> pd.DataFrame:
+    """
+    Normalizes the misinfo dataset CSV files to contain a text and label column. A '1' label value indicates a true piece of
+    text while a '0' label value indicates a false piece of text.
+    
+    Parameters:
+        file_path: The filepath of the CSV file to normalize
+        
+    Returns:
+        pd.Dataframe: A normalized dataframe containing only text and labels column
+    """
     df = pd.read_csv(file_path, usecols=["text"])
     
     label_value = file_path[25:]
@@ -20,6 +30,16 @@ def normalize_misinfo_dataset(file_path: str) -> pd.DataFrame:
     return df
 
 def normalize_fakenewsnet_dataset(file_path: str) -> pd.DataFrame:
+    """    
+    Normalizes the fakenewnet dataset CSV files to contain a text and label column. A '1' label value indicates a true piece of
+    text while a '0' label value indicates a false piece of text.
+    
+    Parameters:
+        file_path: The filepath of the CSV file to normalize
+        
+    Returns:
+        pd.Dataframe: A normalized dataframe containing only text and labels column
+    """
     df = pd.read_csv(file_path, usecols=["title"])
     df.rename(columns={"title": "text"}, inplace=True)
     
@@ -40,7 +60,17 @@ def normalize_fakenewsnet_dataset(file_path: str) -> pd.DataFrame:
     return df
 
 def normalize_liar_dataset(file_path: str) -> pd.DataFrame:
-    # CHATGPT CITATION FOR HELPING TO READ .TSV FILES
+    """    
+    Normalizes the liar dataset TSV files to contain a text and label column. A '1' label value indicates a true piece of
+    text while a '0' label value indicates a false piece of text.
+    
+    Parameters:
+        file_path: The filepath of the TSV file to normalize
+        
+    Returns:
+        pd.Dataframe: A normalized dataframe containing only text and labels column
+    """
+    # CHAT GPT CITATION #1 FOR LINE 74
     df = pd.read_csv(file_path, sep="\t", header=None)
     column_names = ["_", "label", "text", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_", "_"]
     df.columns = column_names
